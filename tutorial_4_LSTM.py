159ed77aa36c792a4e946bfaab9d145c49ae0641
kimseongjung <plznw4me@naver.com>
2017-11-13 13:47:12 +0900

    add tutorial_4

--- tutorial_4_LSTM.py
+++ tutorial_4_LSTM.py
@@ -10,3 +10,81 @@ import cntk.tests.test_utils
 cntk.tests.test_utils.set_device_from_pytest_env() # (only needed for our build system)
 
 %matplotlib inline
+
+isFast=True
+def create_model(x):
+    """Create the model for time series prediction"""
+    with C.layers.default_options(initial_state = 0.1):
+        m = C.layers.Recurrence(C.layers.LSTM(N))(x)
+        m = C.sequence.last(m)
+        m = C.layers.Dropout(0.2, seed=1)(m)
+        m = C.layers.Dense(1)(m)
+        return m
+
+
+def next_batch(x, y, ds):
+    """get the next batch to process"""
+
+    def as_batch(data, start, count):
+        part = []
+        for i in range(start, start + count):
+            part.append(data[i])
+        return np.array(part)
+
+    for i in range(0, len(x[ds])-BATCH_SIZE, BATCH_SIZE):
+        yield as_batch(x[ds], i, BATCH_SIZE), as_batch(y[ds], i, BATCH_SIZE)
+
+
+# Training parameters
+
+TRAINING_STEPS = 10000
+BATCH_SIZE = 100
+EPOCHS = 10 if isFast else 100
+
+
+x_axes = [C.Axis.default_batch_axis(), C.Axis.default_dynamic_axis()]
+C.input_variable(1, dynamic_axes=x_axes)
+
+
+# input sequences
+x = C.sequence.input_variable(1)
+
+# create the model
+z = create_model(x)
+
+# expected output (label), also the dynamic axes of the model output
+# is specified as the model of the label input
+l = C.input_variable(1, dynamic_axes=z.dynamic_axes, name="y")
+
+# the learning rate
+learning_rate = 0.02
+lr_schedule = C.learning_rate_schedule(learning_rate, C.UnitType.minibatch)
+
+# loss function
+loss = C.squared_error(z, l)
+
+# use squared error to determine error for now
+error = C.squared_error(z, l)
+
+# use fsadagrad optimizer
+momentum_time_constant = C.momentum_as_time_constant_schedule(BATCH_SIZE / -math.log(0.9))
+learner = C.fsadagrad(z.parameters,
+                      lr = lr_schedule,
+                      momentum = momentum_time_constant,
+                      unit_gain = True)
+
+trainer = C.Trainer(z, (loss, error), [learner])
+
+
+# train
+loss_summary = []
+start = time.time()
+for epoch in range(0, EPOCHS):
+    for x1, y1 in next_batch(X, Y, "train"):
+        trainer.train_minibatch({x: x1, l: y1})
+    if epoch % (EPOCHS / 10) == 0:
+        training_loss = trainer.previous_minibatch_loss_average
+        loss_summary.append(training_loss)
+        print("epoch: {}, loss: {:.5f}".format(epoch, training_loss))
+
+print("training took {0:.1f} sec".format(time.time() - start))
