352ca5cfea350453d1bb567aaa2d7f8eb7dbb658
kimseongjung <plznw4me@naver.com>
2017-11-14 16:37:23 +0900

    tutorial_4_solar_csv

---
+++ tutorial_5_LSTM_ATIS.py
@@ -0,0 +1,7 @@
+import math
+import numpy as np
+
+import cntk as C
+import cntk.tests.test_utils
+cntk.tests.test_utils.set_device_from_pytest_env() # (only needed for our build system)
+C.cntk_py.set_fixed_random_seed(1) # fix a random seed for CNTK components
