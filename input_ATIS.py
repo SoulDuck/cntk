40b947db816ce2aa3473df1cb55a504b91ae6e71
kimseongjung <plznw4me@naver.com>
2017-11-14 16:32:32 +0900

    tutorial_4_solar_csv

---
+++ input_ATIS.py
@@ -0,0 +1,41 @@
+#-*- coding:utf-8 -*-
+import requests
+import os
+
+def download(url, filename):
+    """ utility function to download a file """
+    response = requests.get(url, stream=True)
+    with open(filename, "wb") as handle:
+        for data in response.iter_content():
+            handle.write(data)
+
+locations = ['Tutorials/SLUHandsOn', 'Examples/LanguageUnderstanding/ATIS/BrainScript']
+
+data = {
+  'train': { 'file': 'atis.train.ctf', 'location': 0 },
+  'test': { 'file': 'atis.test.ctf', 'location': 0 },
+  'query': { 'file': 'query.wl', 'location': 1 },
+  'slots': { 'file': 'slots.wl', 'location': 1 }
+}
+
+for item in data.values():
+    location = locations[item['location']]
+    path = os.path.join('..', location , item['file'])
+    print 'location : ' , location
+    print 'path : ' , path
+    print 'item : ', item['file']
+
+    if os.path.exists(path): #cntk 을 바로 다운로드 받았을때를 대비해서
+        print("Reusing locally cached:", item['file'])
+        # Update path
+        item['file'] = path
+
+    elif os.path.exists(os.path.join('./data/ATIS' , item['file'])):# local에서 코드를 짜서 실행할 때를 대비해서
+        print("Reusing locally cached:", item['file'])
+    else:
+        print("Starting download:", item['file'])
+        url = "https://github.com/Microsoft/CNTK/blob/release/2.2/%s/%s?raw=true"%(location, item['file'])
+        print 'url :' ,url
+        download(url, os.path.join('./data/ATIS',item['file']))
+        print("Download completed")
+
