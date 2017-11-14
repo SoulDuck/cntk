7d8274e5f831fbb890db434a03e3abd9ebe131ca
kimseongjung <plznw4me@naver.com>
2017-11-14 14:15:20 +0900

    tutorial_4_solar_csv

--- input_solar.py
+++ input_solar.py
@@ -21,28 +21,38 @@ def generate_solar_data(input_url, time_steps, normalize=1, val_size=0.1, test_s
         print("using cache for ", input_url)
 
     df = pd.read_csv(cache_file, index_col="time", parse_dates=['time'], dtype=np.float32)
+
+
     df["date"] = df.index.date
+
     # normalize data
     df['solar.current'] /= normalize
     df['solar.total'] /= normalize
     # group by day, find the max for a day and add a new column .max
     grouped = df.groupby(df.index.date).max()
-    grouped.columns = ["solar.current.max", "solar.total.max", "date"]
+    print df[:1]
+    print np.shape(df)
+    grouped.columns = ["solar.current.max", "solar.total.max", "date"]# change grouped columns name
+    print grouped[:1]
+    print np.shape(grouped)
     # merge continuous readings and daily max values into a single frame
     df_merged = pd.merge(df, grouped, right_index=True, on="date")
     df_merged = df_merged[["solar.current", "solar.total",
                            "solar.current.max", "solar.total.max"]]
+    print df_merged[:1]
+    print np.shape(df_merged)
+
     # we group by day so we can process a day at a time.
     grouped = df_merged.groupby(df_merged.index.date)
-
-
     per_day = []
     for _, group in grouped:
         per_day.append(group)
 
     # split the dataset into train, validatation and test sets on day boundaries
     val_size = int(len(per_day) * val_size)
+    print 'validation  size {}'.format(val_size)
     test_size = int(len(per_day) * test_size)
+    print 'test size {}'.format(test_size)
     next_val = 0
     next_test = 0
 
@@ -51,6 +61,8 @@ def generate_solar_data(input_url, time_steps, normalize=1, val_size=0.1, test_s
 
     # generate sequences a day at a time
     for i, day in enumerate(per_day):
+
+
         # if we have less than 8 datapoints for a day we skip over the
         # day assuming something is missing in the raw data
         total = day["solar.total"].values
@@ -59,22 +71,35 @@ def generate_solar_data(input_url, time_steps, normalize=1, val_size=0.1, test_s
         if i >= next_val:
             current_set = "val"
             next_val = i + int(len(per_day) / val_size)
+
         elif i >= next_test:
             current_set = "test"
             next_test = i + int(len(per_day) / test_size)
         else:
             current_set = "train"
         max_total_for_day = np.array(day["solar.total.max"].values[0])
+        print 'total : {}'.format(len(total))
         for j in range(2, len(total)):
+            #print 'current set : {}'.format(current_set)
             result_x[current_set].append(total[0:j])
             result_y[current_set].append([max_total_for_day])
             if j >= time_steps:
                 break
+
+
     # make result_y a numpy array
     for ds in ["train", "val", "test"]:
         result_y[ds] = np.array(result_y[ds])
-    return result_x, result_y
 
+    print 'n train x : ',np.shape(result_x['train'])
+    print 'n test  x  : ',np.shape(result_x['val'])
+    print 'n valication x : ',np.shape(result_x['test'])
+    print 'n train y: ',np.shape(result_y['train'])
+    print 'n test  y  : ',np.shape(result_y['val'])
+    print 'n validation  y  : ',np.shape(result_y['test'])
+
+
+    return result_x, result_y
 
 # We keep upto 14 inputs from a day
 TIMESTEPS = 14
--- tutorial_4_LSTM_predict_solar.py
+++ tutorial_4_LSTM_predict_solar.py
@@ -9,9 +9,69 @@ import numpy as np
 import os
 import pandas as pd
 import time
-
+import input_solar
 import cntk as C
 import cntk.tests.test_utils
 cntk.tests.test_utils.set_device_from_pytest_env() # (only needed for our build system)
 
 
+TIMESTEPS=14
+# process batches of 10 days
+BATCH_SIZE = TIMESTEPS * 10
+
+NORMALIZE = 20000
+X, Y = input_solar.generate_solar_data("https://www.cntk.ai/jup/dat/solar.csv",
+                           TIMESTEPS, normalize=NORMALIZE)
+def next_batch(x, y, ds):
+    """get the next batch for training"""
+
+    def as_batch(data, start, count):
+        return data[start:start + count]
+
+    for i in range(0, len(x[ds]), BATCH_SIZE):
+        yield as_batch(X[ds], i, BATCH_SIZE), as_batch(Y[ds], i, BATCH_SIZE)
+
+
+
+#Specify the internal-state dimensions of the LSTM cell
+H_DIMS = 15
+def create_model(x):
+    """Create the model for time series prediction"""
+    with C.layers.default_options(initial_state = 0.1):
+        m = C.layers.Recurrence(C.layers.LSTM(H_DIMS))(x)
+        m = C.sequence.last(m)
+        m = C.layers.Dropout(0.2)(m)
+        m = C.layers.Dense(1)(m)
+        return m
+
+
+def training():
+    # input sequences
+    x = C.sequence.input_variable(1)
+
+    # create the model
+    z = create_model(x)
+
+    # expected output (label), also the dynamic axes of the model output
+    # is specified as the model of the label input
+    l = C.input_variable(1, dynamic_axes=z.dynamic_axes, name="y")
+
+    # the learning rate
+    learning_rate = 0.005
+    lr_schedule = C.learning_rate_schedule(learning_rate, C.UnitType.minibatch)
+
+    # loss function
+    loss = C.squared_error(z, l)
+
+    # use squared error to determine error for now
+    error = C.squared_error(z, l)
+
+    # use adam optimizer
+    momentum_time_constant = C.momentum_as_time_constant_schedule(BATCH_SIZE / -math.log(0.9))
+    learner = C.fsadagrad(z.parameters,
+                          lr = lr_schedule,
+                          momentum = momentum_time_constant)
+    trainer = C.Trainer(z, (loss, error), [learner])
+
+if __name__ == '__main__':
+    training()
