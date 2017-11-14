a395793f8ba8ad60fd7e1c1671f4c00282817cea
kimseongjung <plznw4me@naver.com>
2017-11-14 17:18:56 +0900

    tutorial_4_solar_csv

--- tutorial_5_LSTM_ATIS.py
+++ tutorial_5_LSTM_ATIS.py
@@ -5,3 +5,26 @@ import cntk as C
 import cntk.tests.test_utils
 cntk.tests.test_utils.set_device_from_pytest_env() # (only needed for our build system)
 C.cntk_py.set_fixed_random_seed(1) # fix a random seed for CNTK components
+
+# number of words in vocab, slot labels, and intent labels
+vocab_size = 943 ; num_labels = 129 ; num_intents = 26
+
+# model dimensions
+input_dim  = vocab_size
+label_dim  = num_labels
+emb_dim    = 150
+hidden_dim = 300
+
+# Create the containers for input feature (x) and the label (y)
+x = C.sequence.input_variable(vocab_size)
+y = C.sequence.input_variable(num_labels)
+
+print 'input dimension {}'.format(x)
+print 'label dimension {}'.format(y)
+def create_model():
+    with C.layers.default_options(initial_state=0.1):
+        return C.layers.Sequential([
+            C.layers.Embedding(emb_dim, name='embed'),
+            C.layers.Recurrence(C.layers.LSTM(hidden_dim), go_backwards=False),
+            C.layers.Dense(num_labels, name='classify')
+        ])
