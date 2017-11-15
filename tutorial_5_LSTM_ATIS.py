1f79752d7c59d0da1004d38ddc1caaef4ac3cd6a
kimseongjung <plznw4me@naver.com>
2017-11-15 16:07:49 +0900

    tutorial_4_solar_csv

--- tutorial_5_LSTM_ATIS.py
+++ tutorial_5_LSTM_ATIS.py
@@ -39,7 +39,6 @@ print(z.classify.b.value)
 z = create_model()
 print(z(x).embed.E.shape)
 
-
 def create_criterion_function(model):
     labels = C.placeholder(name='labels')
     ce   = C.cross_entropy_with_softmax(model, labels)
@@ -47,4 +46,4 @@ def create_criterion_function(model):
     return C.combine ([ce, errs]) # (features, labels) -> (loss, metric)
 
 criterion = create_criterion_function(create_model())
-criterion.replace_placeholders({criterion.placeholders[0]: C.sequence.input_variable(num_labels)})
+print criterion.replace_placeholders({criterion.placeholders[0]: C.sequence.input_variable(num_labels)})
