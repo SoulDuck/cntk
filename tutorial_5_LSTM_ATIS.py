e84fbdba6b08778b8afa68ba388a99d9d8b14f0b
kimseongjung <plznw4me@naver.com>
2017-11-15 16:10:49 +0900

    tutorial_4_solar_csv

--- tutorial_5_LSTM_ATIS.py
+++ tutorial_5_LSTM_ATIS.py
@@ -53,3 +53,58 @@ def create_criterion_function_preferred(model, labels):
     ce   = C.cross_entropy_with_softmax(model, labels)
     errs = C.classification_error(model, labels)
     return ce, errs # (model, labels) -> (loss, error metr
+
+def train(reader, model_func, max_epochs=10):
+
+    # Instantiate the model function; x is the input (feature) variable
+    model = model_func(x)
+
+    # Instantiate the loss and error function
+    loss, label_error = create_criterion_function_preferred(model, y)
+
+    # training config
+    epoch_size = 18000        # 18000 samples is half the dataset size
+    minibatch_size = 70
+
+    # LR schedule over epochs
+    # In CNTK, an epoch is how often we get out of the minibatch loop to
+    # do other stuff (e.g. checkpointing, adjust learning rate, etc.)
+    lr_per_sample = [3e-4]*4+[1.5e-4]
+    lr_per_minibatch = [lr * minibatch_size for lr in lr_per_sample]
+    lr_schedule = C.learning_rate_schedule(lr_per_minibatch, C.UnitType.minibatch, epoch_size)
+
+    # Momentum schedule
+    momentum_as_time_constant = C.momentum_as_time_constant_schedule(700)
+
+    # We use a the Adam optimizer which is known to work well on this dataset
+    # Feel free to try other optimizers from
+    # https://www.cntk.ai/pythondocs/cntk.learner.html#module-cntk.learner
+    learner = C.adam(parameters=model.parameters,
+                     lr=lr_schedule,
+                     momentum=momentum_as_time_constant,
+                     gradient_clipping_threshold_per_sample=15,
+                     gradient_clipping_with_truncation=True)
+
+    # Setup the progress updater
+    progress_printer = C.logging.ProgressPrinter(tag='Training', num_epochs=max_epochs)
+
+    # Uncomment below for more detailed logging
+    #progress_printer = ProgressPrinter(freq=100, first=10, tag='Training', num_epochs=max_epochs)
+
+    # Instantiate the trainer
+    trainer = C.Trainer(model, (loss, label_error), learner, progress_printer)
+
+    # process minibatches and perform model training
+    C.logging.log_number_of_parameters(model)
+
+    t = 0
+    for epoch in range(max_epochs):         # loop over epochs
+        epoch_end = (epoch+1) * epoch_size
+        while t < epoch_end:                # loop over minibatches on the epoch
+            data = reader.next_minibatch(minibatch_size, input_map={  # fetch minibatch
+                x: reader.streams.query,
+                y: reader.streams.slot_labels
+            })
+            trainer.train_minibatch(data)               # update model with it
+            t += data[y].num_samples                    # samples so far
+        trainer.summarize_training_progress()
